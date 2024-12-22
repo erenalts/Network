10b9e3a49504a79117db3b958f17a73a8e772728
erenalts <erenaltas48@gmail.com>
2024-12-23 02:48:52 +0300

    Add files via upload

---
+++ Encapsulation/m/merkez.java
@@ -0,0 +1,19 @@
+package m;
+
+public class merkez {
+
+	public static void main(String[] args) {
+		Kisi kisi1 = new Kisi();
+
+		kisi1.setAd("Melih ");
+		kisi1.setSoyad("Melih ");
+		kisi1.setYas(12);
+		                                                                                       
+		
+		
+		
+		kisi1.yazdir(); 
+
+	}
+
+}
